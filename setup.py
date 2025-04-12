476cbaa63f8dd89e2f8461e125dda6bc796445e0
Navin S <117109209+NavinSivakumar07@users.noreply.github.com>
2025-04-12 11:43:18 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Navin',
+    author_email= 'navinsivakumar07@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
